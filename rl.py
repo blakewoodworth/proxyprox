400dbf467bb063f6ce514a9db805cbaef49e02e5
Blake Woodworth <blakewoodworth@gmail.com>
2025-01-30 16:25:04 -0500

    running RL experiments

--- rl.py
+++ rl.py
@@ -3,6 +3,41 @@ import gymnasium as gym
 import numpy as np
 import argparse
 from tqdm import tqdm
+from matplotlib import pyplot as plt
+
+
+class DoublePendulum:
+
+    def __init__(self, n_actions=25):
+        self.env = gym.make('InvertedDoublePendulum-v5')
+        self.n_actions = n_actions
+        self.action_map = np.linspace(-1.,1.,self.n_actions)[:,None]
+
+    def reset(self):
+        return self.env.reset()
+
+    def step(self, action):
+        return self.env.step(action)
+
+
+class SinglePendulum:
+
+    def __init__(self, n_actions=25):
+        self.env = gym.make('InvertedPendulum-v5')
+        self.n_actions = n_actions
+        self.action_map = np.linspace(-3.,3.,self.n_actions)[:,None]
+
+    def obs_mapping(self, obs):
+        return [obs[0], np.sin(obs[1]), 0., np.cos(obs[1]), 0., obs[2], obs[3], 0., 0.]
+
+    def reset(self):
+        obs, x = self.env.reset()
+        return self.obs_mapping(obs), x
+
+    def step(self, action):
+        obs, reward, terminated, truncated, x = self.env.step(action)
+        return self.obs_mapping(obs), reward, terminated, truncated, x
+        
 
 class Agent(torch.nn.Module):
     def __init__(self, args):
@@ -49,7 +84,7 @@ class Trajectory:
             done = False
             episode_rewards = []
             episode_values = []
-            obs, _ = env.env.reset()
+            obs, _ = env.reset()
             while not done:
                 episode_t += 1
 
@@ -62,7 +97,7 @@ class Trajectory:
                 episode_values.append(value.item())
                 action = env.action_map[action_idx]
 
-                obs, reward, terminated, truncated, _ = env.env.step(action)
+                obs, reward, terminated, truncated, _ = env.step(action)
 
                 total_episode_reward += reward
                 episode_rewards.append(reward)
@@ -133,7 +168,7 @@ class ProxyProxRL:
         self.proxy_env = proxy_env
         self.args = args
 
-    def calculate_loss(self, batch_obs, batch_actions, batch_rtg, batch_logprob, batch_advantage):
+    def calculate_rl_loss(self, batch_obs, batch_actions, batch_rtg, batch_logprob, batch_advantage):
         # calculate ppo-clip loss
         _, new_logprob, new_entropy, new_value = self.agent.action_and_value(batch_obs, batch_actions)
         ratio = (new_logprob - batch_logprob).exp()
@@ -156,7 +191,7 @@ class ProxyProxRL:
             grads[name] = torch.zeros(parameters.size())
 
         for i in range(trajectory.steps_per_epoch):
-            loss = self.calculate_loss(*trajectory.get_batch())
+            loss = self.calculate_rl_loss(*trajectory.get_batch())
             self.agent.zero_grad()
             loss.backward()
 
@@ -167,187 +202,222 @@ class ProxyProxRL:
 
     def get_bias_correction(self):
         
-        target_grads = self.calculate_grad(Trajectory(self.target_env, self.agent, self.args))
+        target_trajectory = Trajectory(self.target_env, self.agent, self.args)
+        target_grads = self.calculate_grad(target_trajectory)
         proxy_grads = self.calculate_grad(Trajectory(self.proxy_env, self.agent, self.args))
 
         bias_correction = {}
         for name in target_grads:
             bias_correction[name] = target_grads[name] - proxy_grads[name]
 
-        return bias_correction
+        return bias_correction, target_trajectory.avg_reward.item()
 
-
-    def train(self):
+    def train(self, do_sgd=False):
         self.optimizers = {}
-        self.schedulers = {}
+        # self.schedulers = {}
         for name, parameters in self.agent.named_parameters():
             self.optimizers[name] = torch.optim.Adam([parameters], lr=self.args['stepsize'])
-            self.schedulers[name] = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizers[name], self.args['n_outer_loops'], 
-                                            eta_min=self.args['stepsize_anneal_factor']*self.args['stepsize'], verbose='deprecated')
+            # self.schedulers[name] = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizers[name], self.args['n_outer_loops'], 
+            #                                 eta_min=self.args['stepsize_anneal_factor']*self.args['stepsize'], verbose='deprecated')
         self.parameter_names = list(self.optimizers.keys())
         
         avg_rewards = []
-        for k in range(1,args['n_outer_loops']+1):
+        n_outer = self.args['n_outer_loops']
+        for k in range(1,n_outer+1):
 
-            target_trajectory = Trajectory(target_env, agent, args)
-            avg_rewards.append(target_trajectory.avg_reward)
+            if do_sgd:
+                target_trajectory = Trajectory(self.target_env, self.agent, self.args)
+                avg_rewards.append(target_trajectory.avg_reward.item())
 
-            target_grad = self.calculate_grad(target_trajectory)
+                for _ in range(target_trajectory.steps_per_epoch):
 
-            proxy_trajectory = Trajectory(proxy_env, agent, args)
+                    loss = self.calculate_rl_loss(*target_trajectory.get_batch())
 
-            proxy_grad = self.calculate_grad(proxy_trajectory)
+                    if loss.item() > 1000:
+                        return None
 
-            # if i_update % 50 == 0:
-            #     print(f"Update {i_update}/{args['n_outer_loops']}: average reward {np.mean(avg_rewards):.3f}")
-            #     avg_rewards = []
+                    for optimizer in self.optimizers.values():
+                        optimizer.zero_grad()
 
-            # # optimize PPO-clip objective, value function, and entropy loss
-            # for inner_epoch in range(args['inner_epochs']):
+                    loss.backward()
+
+                    for optimizer in self.optimizers.values():
+                        optimizer.step() 
+            else:
+                bias_correction, target_avg_reward = self.get_bias_correction()
+                avg_rewards.append(target_avg_reward)
+                wk = {n: p.detach().clone() for n, p in self.agent.named_parameters()}
                 
-            #     for i in range(trajectory.steps_per_epoch):
+                for i in range(self.args['n_inner_loops']):
 
-            #         batch = trajectory.get_batch()
-            #         loss = self.calculate_loss(*batch)
+                    proxy_trajectory = Trajectory(self.proxy_env, self.agent, self.args)
+                    for _ in range(self.args['n_inner_inner_epochs']):
+                        for _ in range(proxy_trajectory.steps_per_epoch):
 
-            #         for name in parameter_names:
-            #             optimizers[name].zero_grad()
-            #             target_grads[name] = torch.zeros(target_grads[name].size())
+                            loss = self.calculate_rl_loss(*proxy_trajectory.get_batch())
 
-            #         loss.backward()
+                            if loss.item() > 1000:
+                                return None
 
-            #         for name in parameter_names:
-            #             optimizers[name].step()
-            # for name in parameter_names:
-            #     schedulers[name].step()
+                            for optimizer in self.optimizers.values():
+                                optimizer.zero_grad()
 
-# def proxy_ppo_clip(target_env, proxy_env, agent, args):
-#     optimizers = {}
-#     schedulers = {}
-#     target_grads = {}
-#     verb = 'deprecated' if int(torch.__version__[0])>1 else False
-#     for name, parameters in agent.named_parameters():
-#         optimizers[name] = torch.optim.Adam([parameters], lr=args['stepsize'])
-#         schedulers[name] = torch.optim.lr_scheduler.CosineAnnealingLR(optimizers[name], 
-#             args['n_updates'], eta_min=args['stepsize_anneal_factor']*args['stepsize'], verbose=verb)
-#         target_grads[name] = torch.zeros(parameters.size())
-#     parameter_names = list(optimizers.keys())
-    
-#     avg_rewards = []
-#     for i_update in range(1,args['n_outer_loops']+1):
+                            loss.backward()
 
-#         target_trajectory = Trajectory(target_env, agent, args)
-#         avg_rewards.append(target_trajectory.avg_reward)
+                            for name, parameters in self.agent.named_parameters():
+                                parameters.grad += (parameters - wk[name]) / self.args['eta']
+                                parameters.grad += bias_correction[name]
 
-#         if i_update % 50 == 0:
-#             print(f"Update {i_update}/{args['n_outer_loops']}: average reward {np.mean(avg_rewards):.3f}")
-#             avg_rewards = []
+                            for optimizer in self.optimizers.values():
+                                optimizer.step() 
 
-#         # optimize PPO-clip objective, value function, and entropy loss
-#         for inner_epoch in range(args['inner_epochs']):
-            
-#             for i in range(trajectory.steps_per_epoch):
+            # for scheduler in self.schedulers.values():
+            #     scheduler.step()
 
-#                 batch_obs, batch_actions, batch_rtg, batch_logprob, batch_advantage = trajectory.get_batch()
-
-#                 # calculate ppo-clip loss
-#                 _, new_logprob, new_entropy, new_value = agent.action_and_value(batch_obs, batch_actions)
-#                 ratio = (new_logprob - batch_logprob).exp()
-
-#                 ppo_clip_loss1 = -ratio * batch_advantage
-#                 ppo_clip_loss2 = -torch.clamp(ratio, 1-args['clip_epsilon'], 1+args['clip_epsilon'])
-#                 ppo_clip_loss = torch.max(ppo_clip_loss1, ppo_clip_loss2).mean()
-
-#                 # calculate value loss
-#                 # value_loss = ((new_value - batch_rtg)**2).mean()
-#                 value_loss = torch.nn.functional.smooth_l1_loss(new_value.view(-1), batch_rtg)
-
-#                 # calculate entropy loss
-#                 entropy_loss = -new_entropy.mean()
-
-#                 all_loss = ppo_clip_loss + args['value_coefficient']*value_loss + args['entropy_coefficient']*entropy_loss
-
-#                 for name in parameter_names:
-#                     optimizers[name].zero_grad()
-#                     target_grads[name] = torch.zeros(target_grads[name].size())
-
-#                 all_loss.backward()
-
-#                 # for name, parameters in agent.named_parameters():
-#                 #     print(name, parameters.grad)
-#                 #     print('\n\n')
-
-#                 # explode()
-
-#                 for name in parameter_names:
-#                     optimizers[name].step()
-#         for name in parameter_names:
-#             schedulers[name].step()
-
-class DoublePendulum:
-
-    def __init__(self, n_actions=25):
-        self.env = gym.make('InvertedDoublePendulum-v5')
-        self.n_actions = n_actions
-        self.action_map = np.linspace(-1.,1.,self.n_actions)[:,None]
+        return avg_rewards
 
 
-class SinglePendulum:
-
-    def __init__(self, n_actions=25):
-        self.env = gym.make('InvertedPendulum-v5')
-        self.n_actions = n_actions
-        self.action_map = np.linspace(-3.,3.,self.n_actions)[:,None]
 
 
 
 np.random.seed(1)
-torch.manual_seed(1)
+
+n_actions = 21
+
+target_env = DoublePendulum(n_actions=n_actions)
+proxy_env = SinglePendulum(n_actions=n_actions)
 
 args = {
-    'n_actions': 21,
-    'hidden_dim': 128,
-    'time_steps_per_batch': 800,
+    'n_actions': n_actions,
+    'hidden_dim': 25,
+    'time_steps_per_batch': 400,
     'max_time_steps_per_episode': 200,
-    'n_outer_loops': 100,
-    'inner_epochs': 4,
-    'batch_size': 250,
-    'clip_epsilon': 0.2,
-    'value_coefficient': 0.01,
-    'entropy_coefficient': 0.005,
-    'gamma': 0.975,
-    'gae_lambda': 0.975,
-    'stepsize': 5e-4,
-    'stepsize_anneal_factor': 1e-1
+    'n_outer_loops': 50,
+    'n_inner_loops': 4,
+    'n_inner_inner_epochs': 1,
+    'batch_size': 200,
+    'clip_epsilon': 0.15,
+    'value_coefficient': 0.03,
+    'entropy_coefficient': 0.001,
+    'gamma': 0.99,
+    'gae_lambda': 0.99,
+    'stepsize': 7e-3,
+    'stepsize_anneal_factor': 1e-1,
+    'eta': 1.,
+    'observation_shape': target_env.env.observation_space.shape,
+    'action_n': target_env.n_actions
 }
 
-target_env = DoublePendulum(n_actions=args['n_actions'])
-proxy_env = SinglePendulum(n_actions=target_env.n_actions)
+n_reps = 4
 
-args['observation_shape'] = target_env.env.observation_space.shape
-args['action_n'] = target_env.n_actions
+lrs = np.power(10, np.linspace(-2.1, -1.5, 5))
+best_reward = -float('inf')
+best_avg_rewards = None
+best_lr = None
+progress_bar = tqdm(total=len(lrs)*n_reps)
+for lr in lrs:
+    args['stepsize'] = lr
 
-agent = Agent(args)
+    avg_avg_rewards = None
+    reward = 0.
+    n_converged = 0
+    for _ in range(n_reps):
+        torch.manual_seed(1)
+        agent = Agent(args)
+        avg_rewards = ProxyProxRL(agent, target_env, proxy_env, args).train(do_sgd=True)
+        if avg_rewards is not None:
+            n_converged += 1
+            reward += np.mean(np.array(avg_rewards)[-10:])
+            if avg_avg_rewards is None:
+                avg_avg_rewards = np.array(avg_rewards)
+            else:
+                avg_avg_rewards += np.array(avg_rewards)
+        progress_bar.update()
+    if n_converged > 0:
+        reward /= n_converged
+        avg_avg_rewards /= n_converged
+    else:
+        reward = -float('inf')
+    if reward > best_reward:
+        best_reward = reward
+        best_avg_rewards = avg_avg_rewards
+        best_lr = lr
 
-# ppo_clip(target_env, agent, args)
+print(f'\n\nbest lr: {best_lr} | best sgd reward: {best_reward}')
 
-proxyprox = ProxyProxRL(agent, target_env, proxy_env, args)
-proxyprox.train()
+sgd_avg_rewards = best_avg_rewards
 
-# env = gym.make(args['env_name'], render_mode='rgb_array')
-# env = gym.wrappers.RecordVideo(env, f"videos/{args['env_name']}")
 
-# obs, _ = env.reset()
-# done = False
-# rewards = 0.
-# while not done:
-#     with torch.no_grad():
-#         action = agent.action_and_value(torch.Tensor(obs))[0]
-#     obs, reward, terminated, truncated, _ = env.step(action.numpy())
-#     rewards += reward
-#     done = terminated or truncated
-# print(f'Final simulation total reward: {rewards}')
-# env.close()
+info = []
+# lrs = np.power(10, np.linspace(-3, -1.5, 10))
+# etas = np.power(10, np.linspace(-1., 1., 10))
+
+pairs = [
+    (0.002, 5),
+    (0.002, 10),
+    (0.002, 20),
+    (0.002, 40),
+    (0.004, 1),
+    (0.004, 4),
+    (0.004, 8),
+    (0.004, 12),
+    (0.008, 1),
+    (0.008, 4),
+    (0.008, 8),
+    (0.008, 12),
+]
+progress_bar = tqdm(total=len(pairs)*n_reps)
+
+for lr, eta in pairs:
+    args['stepsize'] = lr
+    args['eta'] = eta
+
+    avg_avg_rewards = None
+    reward = 0.
+    n_converged = 0
+    for _ in range(n_reps):
+        torch.manual_seed(1)
+        agent = Agent(args)
+        avg_rewards = ProxyProxRL(agent, target_env, proxy_env, args).train()
+        if avg_rewards is not None:
+            n_converged += 1
+            reward += np.mean(np.array(avg_rewards)[-10:])
+            if avg_avg_rewards is None:
+                avg_avg_rewards = np.array(avg_rewards)
+            else:
+                avg_avg_rewards += np.array(avg_rewards)
+        progress_bar.update()
+    if n_converged > 0:
+        info.append((lr, eta, reward/n_converged, avg_avg_rewards/n_converged))
+        print(f'lr: {lr:.4f}, eta: {eta:.2f}, average reward: {reward/n_converged:.2f}')
+
+n_best = 5
+info.sort(key=lambda x: x[2], reverse=True)
+
+print('\n\n')
+for i in range(n_best):
+    lr, eta, end, avg_rewards = info[i]
+    print(f'{i}th best | lr: {lr} | eta: {eta} | end reward: {end}')
+
+plt.figure()
+plt.title('Target: Double Pendulum, Proxy: Single Pendulum')
+plt.plot(sgd_avg_rewards, label='SGD baseline')
+for i in range(n_best):
+    lr, eta, _, avg_rewards = info[i]
+    plt.plot(avg_rewards, label=f'lr={lr}, eta={eta}')
+plt.legend()
+plt.savefig('plots/pendulum-rl.pdf')
+plt.show()
+
+
+
+
+
+
+
+
+
+
 
 
 
